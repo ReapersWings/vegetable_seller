5d385894ed3a3264c544c95586b228355bd00519
ReapersWings <ooi98872@gmail.com>
2024-08-13 10:24:45 +0800

    two

--- app/Http/Controllers/user_controller.php
+++ app/Http/Controllers/user_controller.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\emailverify;
 use App\Models\products;
-use Illuminate\Container\Attributes\Auth;
+//use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Validation\Rule;
 
 class user_controller extends Controller
 {
@@ -13,17 +16,44 @@ class user_controller extends Controller
             'data'=>products::paginate(6)
         ]);
     }
+   
     public function login(){
         return view('login');
     }
     public function register(){
         return view('register');
     }
-    public function f_login(){
-        
+    public function f_register(Request $request){
+        $formregister=$request->validate([
+            'name'=>['required',Rule::unique('users','name')],
+            'password'=>'requred|confirmed',
+            'email'=>['required',Rule::unique('users','email')]
+        ]);
+        auth()->login($formregister);
+        $formregister['email_verify_token']=rand(100000,999999);
+        Mail::to($request->email)->send(new emailverify($formregister));
+        return redirect()->name('')->with('message','please verify your email');
+    }
+    public function f_login(Request $request){
+        $formlogin=$request->validate([
+            'name'=>'required',
+            'password'=>'required'
+        ]);
+        if (auth()->attempt($formlogin)) {
+            return redirect()->name('main')->with('message','login successful');
+        }else{
+            return redirect()->name('login')->with('message','Login Failed');
+        }
     }
     public function f_logout(){
-        Auth::logout();
+        auth()->logout();
         return redirect()->route('login')->with('message','logout successful!');
     }
+    //$ git commit -m 'version'
+    // git push -u origin main
+
+    //commit download git hub file
+    //git clone 'link'
+    //composer install
+    //copy env.example and rename .env
 }
---
+++ resources/views/email/emailverify.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Email Verify</h1>
+    <p>{{ $data->email_verify_token }}</p>
+</body>
+</html>
