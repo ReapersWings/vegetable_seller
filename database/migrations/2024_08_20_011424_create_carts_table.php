81c6e3f79bced16c5e4137bd46d86cfa604181cb
ReapersWings <147372252+ReapersWings@users.noreply.github.com>
2025-07-24 13:24:07 +0800

    database sqlite

--- database/migrations/2024_08_20_011424_create_carts_table.php
+++ database/migrations/2024_08_20_011424_create_carts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('carts', function (Blueprint $table) {
-            $table->bigInteger('c_id')->primary()->autoIncrement()->unsigned();
+            $table->bigIncrements('c_id');
             $table->integer('checkout_id')->nullable();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('product_id')->constrained('products','id')->onDelete('cascade');
