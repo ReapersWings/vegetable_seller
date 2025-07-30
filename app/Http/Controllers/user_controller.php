84971b18613a06923bfb6ccf355b3e31e762322a
ReapersWings <147372252+ReapersWings@users.noreply.github.com>
2025-07-30 08:39:31 +0800

    Test 2

--- app/Http/Controllers/user_controller.php
+++ app/Http/Controllers/user_controller.php
@@ -66,7 +66,6 @@ class user_controller extends Controller
             'email'=>['required',Rule::unique('users','email')],
             'phone_number'=>['required',Rule::unique('users','phone_number')]
         ]);
-        dd($formregister);
         $data=User::create($formregister);
         Auth::login($data);
         
