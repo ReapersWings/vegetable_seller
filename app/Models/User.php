a8afdea38e57955062c44ff4b8a7d8535344d32f
ReapersWings <147372252+ReapersWings@users.noreply.github.com>
2025-07-30 08:41:15 +0800

    register debug1

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone_number',
         'email_verify_token',
         'f_name',
         'l_name',
